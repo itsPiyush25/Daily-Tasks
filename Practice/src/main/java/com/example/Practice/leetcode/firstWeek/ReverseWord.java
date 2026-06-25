ac4f86250d32531f7facf751e1120bfc292c8e2c
Piyush Jogi <piyush.jogi@neosofttech.com>
2026-06-25 15:18:08 +0530

    added 2 problems- canPlaceFlower & ReverseWord

--- Practice/src/main/java/com/example/Practice/leetcode/firstWeek/ReverseWord.java
+++ Practice/src/main/java/com/example/Practice/leetcode/firstWeek/ReverseWord.java
@@ -1,7 +1,24 @@
 package com.example.Practice.leetcode.firstWeek;
 
+import java.util.Arrays;
+
 public class ReverseWord {
     public static void main(String[] args) {
-        
+       char[] cr =  {'h','e','l','l','o'};
+        reverseString(cr);
+        System.out.println(Arrays.toString(cr));
+    }
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length -1;
+
+        while(left < right){
+            char c = s[left];
+            s[left] = s[right];
+            s[right] = c;
+
+            left++;
+            right--;
+        }
     }
 }
